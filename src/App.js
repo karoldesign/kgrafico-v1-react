a0967845c34651343803d966a9a5af96a8ae1beb
karol <carolina.chamorro@kairosds.com>
2019-02-22 17:30:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div>Esto es un div generado con React</div>;
+  }
+}
+
+export default App;
